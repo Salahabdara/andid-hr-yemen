80084a431957094d4d0e837aabff782c4644f0a7
Salahabdara <officearabi321@gmail.com>
2025-07-24 23:30:33 +0300

    Create Page.js

---
+++ Page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>مرحبًا بك في موقع Andid HR Yemen</h1>
+      <p>هذا هو الإصدار التجريبي الأول للموقع.</p>
+    </div>
+  );
+}
