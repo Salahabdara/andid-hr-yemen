aca0caf4426cf99055aba8a66f47b0d8c61ecb92
Salahabdara <officearabi321@gmail.com>
2025-07-24 22:24:07 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', marginTop: '5rem' }}>
+      <h1>مرحبًا بك في Andid HR Yemen</h1>
+      <p>هذا هو النموذج الأولي للموقع.</p>
+    </div>
+  );
+}
